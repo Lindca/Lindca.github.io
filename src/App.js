ab137dc3b5210b43c9f9147c7fff989e52df3b1f
Lindca <45475418+Lindca@users.noreply.github.com>
2019-06-24 19:49:23 -0700

    commit no engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import NoMatch from './pages/NoMatch';
+import ContactMe from './pages/ContactMe';
+import Projects from './pages/Projects';
 import NavbarComp from './components/Navbar';
-import { CounterProvider } from './context';
 
 // const initialState = { currentUser: {} };
 // const UserContext = React.createContext(initialState);
@@ -18,9 +16,8 @@ function App() {
           <NavbarComp />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route component={NoMatch} />
+            <Route exact path="/" component={Projects} />
+            <Route exact path="/" component={ContactMe} />
           </Switch>
         </div>
       </CounterProvider>
