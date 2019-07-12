0a009fc97cd631bc735cc4060ea0fde04ac5916d
Lindca <45475418+Lindca@users.noreply.github.com>
2019-07-11 19:05:32 -0700

    Updates

---
+++ precache-manifest.4b2554f3472b31b0cdf846aa0cc5552e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3450013949db0cf02e8b63c0d12cbef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed49366174c6c2265a8f",
+    "url": "/static/css/main.16a0cae0.chunk.css"
+  },
+  {
+    "revision": "052d9282a4f5795ca7a7",
+    "url": "/static/js/2.74b12093.chunk.js"
+  },
+  {
+    "revision": "ed49366174c6c2265a8f",
+    "url": "/static/js/main.557f1b4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
